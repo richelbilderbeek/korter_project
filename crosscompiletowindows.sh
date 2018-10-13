214a9c99749cee7d07b0e392a1c4605d039a921a
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-13 14:51:40 +0200

    Use new filenames

--- crosscompiletowindows.sh
+++ crosscompiletowindows.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #From http://richelbilderbeek.nl/CppQtCrosscompileToWindowsExample15.htm
-i686-w64-mingw32.static-qmake-qt5 ToolMutualismBreakdownerDesktop.pro
+i686-w64-mingw32.static-qmake-qt5 Toolkorter_project_desktop.pro
 make
 echo "Done"
