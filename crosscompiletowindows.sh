0d6e6a1ce403e0174d3721a5b1366f7fda969015
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-14 19:14:22 +0200

    Fix script

--- crosscompiletowindows.sh
+++ crosscompiletowindows.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #From http://richelbilderbeek.nl/CppQtCrosscompileToWindowsExample15.htm
-i686-w64-mingw32.static-qmake-qt5 Toolkorter_project_desktop.pro
+i686-w64-mingw32.static-qmake-qt5 korter_project_desktop.pro
 make
 echo "Done"
