82cd61480a49d349d701dcd8a581d476971e6369
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-01 17:49:17 +0100

    Reproduce bug found by Megan

--- korter_project_simulation_test.cpp
+++ korter_project_simulation_test.cpp
@@ -60,3 +60,54 @@ BOOST_AUTO_TEST_CASE(ribi_kp_simulation_increases_number_of__histograms)
   s.go_to_next_generation();
   BOOST_CHECK_EQUAL(2, s.get_trait_histograms().size());
 }
+
+BOOST_AUTO_TEST_CASE(ribi_kp_no_selection_for_max_zero)
+{
+  // We set a fitness of the facilitated plant
+  // We set a zero fitness of the unfacilitated plant
+  // We expect the simulation to go to the trait of the facilitated plant
+
+  using namespace std;
+
+  const fitness_parameters fp(
+    0.7, //fac_opt
+    0.1, //fac_stddev
+    1.0, //fac_max <- awesome fitness here
+    0.3, //unfac_opt
+    0.1, //unfac_stddev
+    0.0  //unfac_max <- there is no fitness here
+  );
+  const parameters p(
+    fp,
+    0.5, //const double init_trait_mean = 0.5,
+    0.1, //const double init_trait_stddev = 0.1,
+    0.01, //const double mut_stddev = 0.01,
+    25, //const int spatial_height = 1,
+    25, //const int spatial_width = 1,
+    100, //const int n_nurse_plants = 0,
+    200, //const int n_seeds = 0,
+    10, //const int n_trait_histogram_bins = 10,
+    0, //const int rng_seed = 0,
+    0.1, //const double trait_histogram_bin_width = 0.1,
+    1000, //const int max_n_generations = 1000,
+    "results.csv" //const std::string& results_filename = "results.csv"
+  );
+  simulation s(p);
+  for (int i = 0; i != 100; ++i) {
+    s.go_to_next_generation();
+  }
+  const auto traits = collect_traits(s.get_grid());
+  // No fitness at trait 0.3
+  const int n_low = count_if(
+    begin(traits), end(traits),
+    [](const double trait) { return trait < 0.5; }
+  );
+  // Awesome fitness at trait 0.7
+  const int n_high = count_if(
+    begin(traits), end(traits),
+    [](const double trait) { return trait >= 0.5; }
+  );
+
+  BOOST_CHECK(n_high > n_low);
+}
+
